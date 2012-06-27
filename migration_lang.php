1220128c3b3fc08e42051d68323b3a8274b37fae
Anthony VEREZ <netantho@gmail.com>
2012-06-27 23:03:33 +0200

    CodeIgniter 2.1.1

---
+++ migration_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ *  @version 2.1.1
+ *
+ */
+
+$lang['migration_none_found']           = "Aucune migration n'a été trouvée.";
+$lang['migration_not_found']            = "Cette migration n'a pas été trouvée.";
+$lang['migration_multiple_version']     = "Ces migrations ont le même numéro de version : %d.";
+$lang['migration_class_doesnt_exist']   = "La classe de migration \"%s\" n'a pas été trouvée.";
+$lang['migration_missing_up_method']    = "Il manque une méthode 'up' à la classe de migration \"%s\".";
+$lang['migration_missing_down_method']  = "Il manque une méthode 'down' à la classe de migration \"%s\".";
+$lang['migration_invalid_filename']     = "La migration \"%s\" a un nom de fichier incorrect.";
+
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/french/migration_lang.php */
+
